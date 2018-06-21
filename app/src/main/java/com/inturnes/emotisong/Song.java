7032f56199a71d6bde7829b2756d73da7c524a1c
Rosemary Fortanely <elizabeth.fortanely@gmail.com>
2018-06-20 17:52:46 -0700

    beginning work on song analysis

--- app/src/main/java/com/inturnes/emotisong/Song.java
+++ app/src/main/java/com/inturnes/emotisong/Song.java
@@ -1,7 +1,18 @@
 package com.inturnes.emotisong;
 
+//README must comply with https://about.musixmatch.com/apiterms/
 public class Song {
     Emotion emotion;
     int emotionCompatibility;
-    //TODO add information necessary to perform queries on songs in spotify
+
+    public void setEmotion(){
+        //given necessary information (update the data structures associated under song)
+        //look up song lyrics in musixmatch
+        //https://github.com/sachin-handiekar/jMusixMatch
+        //https://stackoverflow.com/questions/16595287/how-to-import-maven-dependency-in-android-studio-intellij
+        //java wrapper for api
+        //text analysis
+        //https://natural-language-understanding-demo.ng.bluemix.net/
+        //feed into ibm bluemix and set the emotion for the song
+    }
 }
