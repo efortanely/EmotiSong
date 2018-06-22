2afd03cd0b0942777ed6405b9bfb9ba5a6e43a78
rofort <rosemary.fortanely@gmail.com>
2018-06-22 00:41:30 -0700

    finished song emotion method

--- app/src/main/java/com/inturnes/emotisong/Emotion.java
+++ app/src/main/java/com/inturnes/emotisong/Emotion.java
@@ -3,8 +3,45 @@ package com.inturnes.emotisong;
 public class Emotion {
     //TODO from microsoft- "digust", "fear", "sadness", "anger", "happiness"
     //TODO from ibm- "digust", "fear", "sadness", "anger", "joy"
+    private double disgust;
+    private double fear;
+    private double sadness;
+    private double anger;
+    private double happiness;
 
     public Emotion(){
+    }
 
+    public Emotion(double disgust, double fear, double sadness, double anger, double happiness){
+        this.disgust = disgust;
+        this.fear = fear;
+        this.sadness = sadness;
+        this.anger = anger;
+        this.happiness = happiness;
+    }
+
+    public void setDisgust(double disgust){
+        this.disgust = disgust;
+    }
+
+    public void setFear(double fear){
+        this.fear = fear;
+    }
+
+    public void setSadness(double sadness){
+        this.sadness = sadness;
+    }
+
+    public void setAnger(double anger){
+        this.anger = anger;
+    }
+
+    public void setHappiness(double happiness){
+        this.happiness = happiness;
+    }
+
+    public double compatibility(Emotion other){
+        return this.disgust * other.disgust + this.fear * other.fear +
+                this.sadness * other.sadness + this.anger * other.anger + this.happiness * other.happiness;
     }
 }
--- app/src/main/java/com/inturnes/emotisong/Song.java
+++ app/src/main/java/com/inturnes/emotisong/Song.java
@@ -3,12 +3,17 @@ package com.inturnes.emotisong;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import com.google.gson.JsonParser;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EmotionOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EmotionResult;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EmotionScores;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.KeywordsOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.TargetedEmotionResults;
 
 import org.jmusixmatch.MusixMatch;
 import org.jmusixmatch.MusixMatchException;
@@ -23,7 +28,7 @@ public class Song extends AsyncTask<String, String, String>{
     private String songName;
     private String artistName;
     private Emotion emotion;
-    private int emotionCompatibility;
+    private double emotionCompatibility;
     private boolean successfullyRanked;
     private Context context;
     private Class<NaturalLanguageUnderstanding> naturalLanguageUnderstandingClass;
@@ -32,11 +37,12 @@ public class Song extends AsyncTask<String, String, String>{
         this.context = context;
         this.songName = songName;
         this.artistName = artistName;
+        this.emotion = new Emotion();
     }
 
     protected String doInBackground(String... urls){
         //TODO temp code
-        setEmotion(new Emotion());
+        setEmotion(new Emotion(0.1,0.2,0.3,0.4,0.5));
         return null;
     }
 
@@ -70,46 +76,67 @@ public class Song extends AsyncTask<String, String, String>{
             lyricsBody = matcher.replaceAll("");
         }
 
-
         NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
                 "2018-03-16",
                 context.getString(R.string.natural_language_understanding_username),
                 context.getString(R.string.natural_language_understanding_password));
         service.setEndPoint(context.getString(R.string.natural_language_understanding_url));
 
-        String text = "IBM is an American multinational technology " +
-                "company headquartered in Armonk, New York, " +
-                "United States, with operations in over 170 countries.";
-
-        EntitiesOptions entitiesOptions = new EntitiesOptions.Builder()
-                .emotion(true)
-                .sentiment(true)
-                .limit(2)
-                .build();
-
-        KeywordsOptions keywordsOptions = new KeywordsOptions.Builder()
-                .emotion(true)
-                .sentiment(true)
-                .limit(2)
+        EmotionOptions emotionOptions = new EmotionOptions.Builder()
                 .build();
 
         Features features = new Features.Builder()
-                .entities(entitiesOptions)
-                .keywords(keywordsOptions)
+                .emotion(emotionOptions)
                 .build();
 
         AnalyzeOptions parameters = new AnalyzeOptions.Builder()
-                .text(text)
+                .text(lyricsBody)
                 .features(features)
                 .build();
 
         AnalysisResults response = service
                 .analyze(parameters)
                 .execute();
-        System.out.println(response);
 
+        EmotionResult emotionResult = response.getEmotion();
 
-        emotionCompatibility = -1;
+        System.out.println(emotionResult);
+
+        String emotionString = emotionResult.toString();
+        pattern = Pattern.compile("\"([a-z]+)\": ([0-9].[0-9]*),?");
+        matcher = pattern.matcher(emotionString);
+        int startingAt = 0;
+        while(matcher.find(startingAt)){
+            String tag = matcher.group(1);
+            double strength = Double.parseDouble(matcher.group(2));
+
+            switch(tag){
+                case "anger":
+                    emotion.setAnger(strength);
+                    break;
+                case "disgust":
+                    emotion.setDisgust(strength);
+                    break;
+                case "fear":
+                    emotion.setFear(strength);
+                    break;
+                case "joy":
+                    emotion.setHappiness(strength);
+                    break;
+                case "sadness":
+                    emotion.setSadness(strength);
+                    break;
+            }
+
+            startingAt = matcher.end();
+        }
+
+        if(startingAt == 0){
+            emotionCompatibility = -1;
+            return;
+        }
+
+        emotionCompatibility = this.emotion.compatibility(faceEmotion);
         successfullyRanked = true;
     }
 }
